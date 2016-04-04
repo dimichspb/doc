f19f7253df81c5aadcfc797040a4a7e4719e7d30
Dmitry Tarantin <dimichspb@gmail.com>
2016-04-04 17:42:29 +0300

    Adding request_to_product table migration

---
+++ migrations/m160404_142304_create_request_to_product.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+use yii\db\Query;
+
+class m160404_142304_create_request_to_product extends Migration
+{
+    public function up()
+    {
+        $this->createTable('{{%request_to_product}}', [
+            'request' => $this->integer(11),
+            'product' => $this->integer(11),
+        ]);
+
+        $this->addForeignKey('fk_request_to_product_request_id', '{{%request_to_product}}', 'request', '{{%request}}', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk_request_to_product_product_id', '{{%request_to_product}}', 'product', '{{%product}}', 'id', 'RESTRICT', 'CASCADE');
+
+        //$this->dropForeignKey('fk_request_product_id', '{{%request}}');
+        //$this->dropColumn('{{%request}}', 'product');
+        $this->dropColumn('{{%request}}', 'quantity');
+    }
+
+    public function down()
+    {
+        $productId = (new Query)
+            ->select('id')
+            ->from('{{%product}}')
+            ->min('id');
+
+        $this->dropTable('{{%request_to_product}}');
+        $this->addColumn('{{%request}}', 'product', $this->integer(11)->notNull()->defaultValue($productId));
+        $this->addColumn('{{%request}}', 'quantity', $this->integer(11)->notNull()->defaultValue(0));
+
+        $this->addForeignKey('fk_request_product_id', '{{%request}}', 'product', '{{%product}}', 'id', 'RESTRICT', 'CASCADE');
+    }
+}
