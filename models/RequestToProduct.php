8ad6e01c9daa8c4359989a88465ce201c370d250
Dmitry Tarantin <dimichspb@gmail.com>
2016-04-14 15:28:11 +0300

    Adding Best Price column at Request view

--- models/RequestToProduct.php
+++ models/RequestToProduct.php
@@ -6,6 +6,7 @@ use Yii;
 use app\models\Request;
 use app\models\Quotation;
 use app\models\QuotationToProduct;
+use yii\db\ActiveQuery;
 
 /**
  * This is the model class for table "request_to_product".
@@ -46,6 +47,7 @@ class RequestToProduct extends \yii\db\ActiveRecord
             'request' => 'Запрос',
             'product' => 'Товар',
             'quantity' => 'Количество',
+            'price' => 'Лучшая цена',
         ];
     }
 
@@ -97,13 +99,51 @@ class RequestToProduct extends \yii\db\ActiveRecord
         return $this->getQuotations()->all();
     }
 
-    public function getQuotationProducts()
+    /**
+     * @return ActiveQuery
+     */
+    public function getActiveQuotations()
     {
-
+        return $this->getQuotations()->where(['status' => Quotation::STATUS_ACTIVE]);
     }
 
+    /**
+     * @return Quotation[]
+     */
+    public function getActiveQuotationsAll()
+    {
+        return $this->getActiveQuotations()->all();
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function getQuotationProducts()
+    {
+        return $this->hasMany(QuotationToProduct::className(), ['quotation' => 'id'])->via('activeQuotations');
+    }
+
+    /**
+     * @return QuotationToProduct[]
+     */
+    public function getQuotationToProductsAll()
+    {
+        return $this->getQuotationProducts()->all();
+    }
+
+    /**
+     * @return QuotationToProduct
+     */
+    public function getQuotationToProductsBest()
+    {
+        return $this->getQuotationProducts()->orderBy(['price' => SORT_ASC])->one();
+    }
+
+    /**
+     * @return float
+     */
     public function getPrice()
     {
-       
+        return $this->getQuotationToProductsBest()->price;
     }
 }
